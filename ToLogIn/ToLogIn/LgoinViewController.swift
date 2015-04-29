03f305395630d1581b6a7f06567b02d49111e135
Casey <casey.j.wilkinson@gmail.com>
2015-04-29 22:26:21 +1000

    Progress
    Need to figure out how to link the input fields with the controller via
    xCode interface…do not want to hard code incase think is not set
    correctly.

---
+++ ToLogIn/ToLogIn/LgoinViewController.swift
@@ -0,0 +1,17 @@
+//
+//  LgoinViewController.swift
+//  ToLogIn
+//
+//  Created by Casey Macrae on 29/04/2015.
+//  Copyright (c) 2015 Casey Macrae. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class LoginViewController: UIViewController, UITextFieldDelegate {
+    
+    
+    
+
+}
---
+++ ToLogIn/ToLogIn/ToLoginView.swift
@@ -0,0 +1,17 @@
+//
+//  ToLoginView.swift
+//  ToLogIn
+//
+//  Created by Casey Macrae on 29/04/2015.
+//  Copyright (c) 2015 Casey Macrae. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+@IBDesignable
+public class ToLogin: UIView {
+
+    
+
+}
